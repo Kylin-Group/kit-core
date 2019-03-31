89957994c530dba17ecd2f760c5b55d94de232cc
Mr.Kevin <kevin@kylin.app>
2019-03-31 13:13:20 +0800

    kit base commit

---
+++ Code/CSharpCode/Kit.Base/Tools/CollectionHelper.cs
@@ -0,0 +1,133 @@
+﻿// ----------------------------------------------------------------------------------------------------
+// Copyright © Guo jin ming. All rights reserved.
+// Homepage: https://kylin.app/
+// E-Mail: kevin@kylin.app
+// ----------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Kit
+{
+    /// <summary>
+    /// 集合或数组的助手类
+    /// </summary>
+    public static class CollectionHelper
+    {
+        /// <summary>
+        /// 升序排列
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="handler"></param>
+        public static void OrderBy<T, TKey>(T[] array, SelectHandler<T, TKey> handler)
+           where TKey : IComparable<TKey>
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+                for (int j = i + 1; j < array.Length; j++)
+                    if (handler(array[i]).CompareTo(handler(array[j])) > 0)
+                    {
+                        var temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+        }
+
+        /// <summary>
+        /// 降序排列
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="handler"></param>
+        public static void OrderByDescending<T, TKey>(T[] array, SelectHandler<T, TKey> handler)
+            where TKey : IComparable
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+                for (int j = i + 1; j < array.Length; j++)
+                    if (handler(array[i]).CompareTo(handler(array[j])) < 0)
+                    {
+                        var temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+        }
+
+        /// <summary>
+        /// 查找委托
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public delegate bool FindHandler<T>(T item);
+        /// <summary>
+        /// 选择委托
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public delegate TKey SelectHandler<TSource, TKey>(TSource source);
+
+        /// <summary>
+        /// 查找
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="handler"></param>
+        /// <returns></returns>
+        public static T Find<T>(T[] array, FindHandler<T> handler)
+        {
+            foreach (var item in array)
+            {
+                //调用委托
+                if (handler(item))
+                    return item;
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// 查找
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="handler"></param>
+        /// <returns></returns>
+        public static T[] FindAll<T>(T[] array, FindHandler<T> handler)
+        {
+            List<T> tempList = new List<T>();
+            foreach (var item in array)
+            {
+                //调用委托
+                if (handler(item))
+                    tempList.Add(item);
+            }
+            return tempList.Count > 0 ? tempList.ToArray() : null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="handler"></param>
+        /// <returns></returns>
+        public static TKey[] Select<T, TKey>(T[] array,
+            SelectHandler<T, TKey> handler)
+        {
+            TKey[] tempArr = new TKey[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArr[i] = handler(array[i]);
+            }
+            return tempArr;
+        }
+
+    }
+
+}
+
+
---
+++ Code/CSharpCode/Kit.Base/Utility/Utility.Data.cs
@@ -0,0 +1,64 @@
+﻿// ----------------------------------------------------------------------------------------------------
+// Copyright © Guo jin ming. All rights reserved.
+// Homepage: https://kylin.app/
+// E-Mail: kevin@kylin.app
+// ----------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Kit
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Utility
+    {
+
+        /// <summary>
+        /// 数据相关处理
+        /// </summary>
+        public static class Data
+        {
+            private readonly static string[] CsvRowSplit = new string[] { "\r\n" };
+            private readonly static string[] CsvColumnSplit = new string[] { "," };
+
+            #region ... Text
+
+            #region ... Csv
+
+            /// <summary>
+            /// 分割数据 安装多行分割成一行一行 CSV 
+            /// </summary>
+            /// <param name="text"></param>
+            /// <returns></returns>
+            public static string[] SplitDataRowByTextCsv(string text)
+            {
+                if (string.IsNullOrEmpty(text))
+                {
+                    return default(string[]);
+                }
+                return text.Split(CsvRowSplit, StringSplitOptions.None);
+            }
+
+            /// <summary>
+            /// 分割数据 按照一行CSV数据切割 单个数据
+            /// </summary>
+            /// <param name="text"></param>
+            /// <returns></returns>
+            public static string[] SplitDataColumnByTextCsvOneRow(string text)
+            {
+                if (string.IsNullOrEmpty(text))
+                {
+                    return default(string[]);
+                }
+                return text.Split(CsvColumnSplit, StringSplitOptions.None);
+            }
+
+            #endregion
+
+            #endregion
+             
+        }
+
+    }
+}
